1c574e4c4f5af8194ea5db39f5b688be6228a071
Woj PL <s16496@pjwstk.edu.pl>
2021-01-13 20:20:27 +0100

    basic usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import "./App.css";
+import React from "react";
+import './App.css';
 
-function App() {}
+const App = () => {
+    async function xor() {
+        const SVM = require('libsvm-js/asm');
+        const svm = new SVM();
+
+        const features = [[0, 0], [1, 1], [1, 0], [0, 1]];
+        const labels = [0, 0, 1, 1];
+        svm.train(features, labels);  // train the model
+        const predictedLabel = svm.predictOne([0.7, 0.8]);
+        console.log(predictedLabel) // 0
+    }
+
+    xor().then(() => console.log('done!'));
+
+    return (
+        <div style={{margin: "40px auto", width: "400px"}}>
+
+        </div>
+    )
+}
 
 export default App;
